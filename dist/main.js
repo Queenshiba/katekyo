e54b49c1b595a469512c12dd99662bf74273ae75
Queenshiba <haruka0111.2a@gmail.com>
2021-10-11 13:56:12 +0900

    add slides

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,25 @@
-var slideIndex = 1;
+
+// click slides
+
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// window.setInterval(function () {
+//     ("a.next").trigger("click");
+// }, 5000);
+
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
@@ -24,3 +31,5 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+// window.setInterval(change, 1000)
+// window.setInterval(plusSlides, 1100)
