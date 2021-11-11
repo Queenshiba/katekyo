0f5cac341bb23add5b616817f115c547d8624ecf
Queenshiba <haruka0111.2a@gmail.com>
2021-11-11 23:56:41 +0900

    cleaned wraps

--- dist/main.js
+++ dist/main.js
@@ -50,5 +50,5 @@ function copyEmailBtn() {
     navigator.clipboard.writeText(copyText.value);
 
     /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    // alert("Copied the text: " + copyText.value);
 }
