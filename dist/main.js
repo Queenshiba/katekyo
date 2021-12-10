5d72c98b6815a91c897e8e0af2a8f0cb86c0ffc3
Queenshiba <haruka0111.2a@gmail.com>
2021-12-10 14:21:28 +0900

    add mobile menu

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,54 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+(function() {
+    'use strict';
 
-hamburger.addEventListener("click", mobileMenu);
+    class Menu {
+        constructor(settings) {
+            this.menuRootNode = settings.menuRootNode;
+            this.isOpened = false;
+        }
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
+        changeMenuState(menuState) {
+            return this.isOpened = !menuState;
+        }
+
+        changeToggleHint(toggleHint, toggleNode) {
+            toggleNode.textContent = toggleHint;
+            return toggleHint;
+        }
+    }
+
+    const menuClassesNames = {
+        rootClass: 'menu',
+        activeClass: 'menu_activated',
+        toggleClass: 'menu__toggle',
+        toggleHintClass: 'menu__toggle-hint'
+    }
+
+    const jsMenuNode = document.querySelector(`.${menuClassesNames.rootClass}`);
+    const demoMenu = new Menu({
+        menuRootNode: jsMenuNode
+    });
+
+    function getCurrentToggleHint(currentMenuState) {
+        return (currentMenuState !== true) ? 'Open menu' : 'Close menu';
+    }
+
+    function toggleMenu(event) {
+
+        let currentMenuState = demoMenu.changeMenuState(demoMenu.isOpened);
+        let toggleHint = getCurrentToggleHint(currentMenuState);
+
+        demoMenu.changeToggleHint(
+            toggleHint,
+            demoMenu.menuRootNode.querySelector(`.${menuClassesNames.toggleHintClass}`)
+        );
+        demoMenu.menuRootNode.classList.toggle(`${menuClassesNames.activeClass}`);
+
+        return currentMenuState;
+    }
+
+    jsMenuNode.querySelector(`.${menuClassesNames.toggleClass}`).addEventListener('click', toggleMenu);
+})();
 
 
 // click slides
